2890f28df30bd7b17deac0794db28286db300ab8
leenhem <leenhem.lh@gmail.com>
2016-09-17 16:52:14 +0800

    success

--- tornado_demo1.py
+++ tornado_demo1.py
@@ -1,46 +1,29 @@
-import tornado.ioloop
 import tornado.web
 import tornado.httpserver
-import tornado.httpclient
 
-class helloworld(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world\n")
 
-class testpage(tornado.web.RequestHandler):
-    def get(self):
-        self.write("You requested the main page")
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/index.html", MainHandler)
+        ]
+        settings = {
+            "template_path": "./static/",
+        }
+        tornado.web.Application.__init__(self, handlers, **settings)
 
-class StoryHandler(tornado.web.RequestHandler):
-    def get(self, story_id):
-        self.write("You requested the story " + story_id)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write('<html><body><form action="/" method="post">'
-                   '<input type="text" name="message">'
-                   '<input type="submit" value="abc">'
-                   '</form></body></html>')
-    def post(self):
-        self.set_header("Content-Type", "text/plain")
-        self.write("You wrote " + self.get_argument("message"))
+        self.render("index.html")
 
-class CustomApplication(tornado.web.Application):
-    def __init__(self):
-        handles = [
-            (r"/", MainHandler),
-            (r"/hello", helloworld),
-            (r"/test", testpage),
-            (r"/story/([0-9]+)", StoryHandler),
-        ]
-        super(CustomApplication,self).__init__(handles)
 
-if __name__ == '__main__':
-    http_server = tornado.httpserver.HTTPServer(CustomApplication)
-    http_server.listen(8088)
-    # tornado.httpclient.AsyncHTTPClient.configure(
-    #     "tornado.simple_httpclient.SimpleAsyncHTTPClient",
-    #     max_clients=2,
-    #     defaults=dict(connect_timeout=float(10), request_timeout=float(100)))
-    # tornado.ioloop.IOLoop.current().start()
+def main():
+    applicaton = Application()
+    http_server = tornado.httpserver.HTTPServer(applicaton)
+    http_server.listen(8888)
+
     tornado.ioloop.IOLoop.instance().start()
+
+# if __name__ == "__main__":
+main()
